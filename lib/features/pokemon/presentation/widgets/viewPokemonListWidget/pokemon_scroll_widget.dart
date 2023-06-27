fe3242492ad277ed329f0c47c5eba5a0054ef84b
johangv <johanalbertog107@gmail.com>
2023-06-26 22:55:26 -0500

    Little last fixes

--- lib/features/pokemon/presentation/widgets/viewPokemonListWidget/pokemon_scroll_widget.dart
+++ lib/features/pokemon/presentation/widgets/viewPokemonListWidget/pokemon_scroll_widget.dart
@@ -75,7 +75,7 @@ class _PokemonScrollWidgetState extends State<PokemonScrollWidget> {
               setState(() {
                 selectedIndex = buttonIndex;
               });
-              context.read<PokemonCubit>().updateSelectedPokemon(selectedIndex);
+              context.read<PokemonCubit>().updateSelectedPokemon(buttonIndex);
             },
           );
         },
